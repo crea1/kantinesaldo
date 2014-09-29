16bd026df5401034cf23b97c349823666402985b
Marius Kristensen <marius.kristensen@gmail.com>
2014-09-29 18:53:57 +0200

    0003 Save threshold in settings screen

--- kantinesaldo-app/src/com/kwc/kantinesaldo/SettingsFragment.java
+++ kantinesaldo-app/src/com/kwc/kantinesaldo/SettingsFragment.java
@@ -5,6 +5,7 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
@@ -14,6 +15,8 @@ import android.widget.Switch;
  * @author Marius Kristensen
  */
 public class SettingsFragment extends DialogFragment {
+
+    private static final String TAG = "kantinesaldo";
 
     private final PreferenceManager preferenceManager;
 
@@ -33,17 +36,20 @@ public class SettingsFragment extends DialogFragment {
         final EditText thresholdText = (EditText) settingsView.findViewById(R.id.threshold);
 
         serviceSwitch.setChecked(preferenceManager.getSavedServiceState());
+        thresholdText.setText(Float.toString(preferenceManager.getBalanceTreshold()));
 
         builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                preferenceManager.setBalanceThreshold(Float.parseFloat(thresholdText.getText().toString()));
+                float threshold = Float.parseFloat(thresholdText.getText().toString());
+                preferenceManager.setBalanceThreshold(threshold);
                 if (preferenceManager.isCardInfoSet()) {
                     preferenceManager.setSavedServiceState(serviceSwitch.isChecked());
                 } else {
                     preferenceManager.setSavedServiceState(false);
                 }
                 BalanceDownloadReceiver.scheduleAlarms(getActivity(), preferenceManager.getSavedServiceState());
+                Log.d(TAG, "Saving (threshold:" + threshold + ", serviceState:" + serviceSwitch.isChecked() + ")");
             }
         });
         builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
